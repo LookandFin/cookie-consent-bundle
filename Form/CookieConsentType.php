04369913d00310f6b2d982867b2cb196e01feca3
Julio <jp@lookandfin.com>
2022-04-06 11:08:21 +0200

    update button / scrollable

--- Form/CookieConsentType.php
+++ Form/CookieConsentType.php
@@ -74,8 +74,10 @@ class CookieConsentType extends AbstractType
 
         $builder->add('use_all_cookies', SubmitType::class, ['label' => 'ch_cookie_consent.use_all_cookies', 'attr' => ['class' => 'btn ch-cookie-consent__btn ch-cookie-consent__btn--secondary']]);
         if ($this->cookieConsentSimplified === false) {
-            $builder->add('save', SubmitType::class, ['label' => 'ch_cookie_consent.save', 'attr' => ['class' => 'btn ch-cookie-consent__btn ch-cookie-consent__btn_save']]);
+            $builder->add('save', SubmitType::class, ['label' => 'ch_cookie_consent.save', 'attr' => ['class' => 'btn ch-cookie-consent__btn ch-cookie-consent__btn--secondary ch-cookie-consent__btn_save']]);
+            $builder->add('use_all_cookies', SubmitType::class, ['label' => 'ch_cookie_consent.use_all_cookies', 'attr' => ['class' => 'btn ch-cookie-consent__btn']]);
         } else {
+            $builder->add('use_all_cookies', SubmitType::class, ['label' => 'ch_cookie_consent.use_all_cookies', 'attr' => ['class' => 'btn ch-cookie-consent__btn ch-cookie-consent__btn--secondary']]);
             $builder->add('use_only_functional_cookies', SubmitType::class, ['label' => 'ch_cookie_consent.use_only_functional_cookies', 'attr' => ['class' => 'btn ch-cookie-consent__btn']]);
         }
         $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
