2adb64b891dbfd03389e2bd646390b1e6d813765
Romain <rn@lookandfin.com>
2026-05-11 10:19:47 +0200

    Update CookieConsentController.php

--- Controller/CookieConsentController.php
+++ Controller/CookieConsentController.php
@@ -16,7 +16,7 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
